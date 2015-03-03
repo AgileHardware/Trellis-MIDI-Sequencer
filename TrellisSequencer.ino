1260de95704e2422adbbe4b126e8c414daf101d6
Karl Sander <kall@kall.ws>
2015-03-03 16:47:40 +0100

    adapt to midi player in adafruit music shield based on example
    not actually tested on the device

--- TrellisSequencer.ino
+++ TrellisSequencer.ino
@@ -9,6 +9,7 @@
 
 #include <Wire.h>
 #include "Adafruit_Trellis.h"
+#include <SoftwareSerial.h>
 
 Adafruit_Trellis matrix0 = Adafruit_Trellis();
 Adafruit_Trellis matrix1 = Adafruit_Trellis();
@@ -16,6 +17,8 @@ Adafruit_Trellis matrix2 = Adafruit_Trellis();
 Adafruit_Trellis matrix3 = Adafruit_Trellis();
 
 Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0, &matrix1, &matrix2, &matrix3);
+
+SoftwareSerial VS1053_MIDI(0, 2);
 
 #define NUM_TRELLIS     4
 #define NUM_COLUMNS     8
@@ -31,6 +34,30 @@ Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0, &matrix1, &matrix2,
 
 // Connect the INT wire from Trellis to pin #5 or change this constant
 #define INTPIN          5
+
+// Pins for Music Maker Shield
+
+#define VS1053_RX  2 // This is the pin that connects to the RX pin on VS1053
+#define VS1053_RESET 9 // This is the pin that connects to the RESET pin on VS1053
+
+// MIDI Data
+
+// See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 31
+#define VS1053_BANK_DEFAULT 0x00
+#define VS1053_BANK_DRUMS1 0x78
+#define VS1053_BANK_DRUMS2 0x7F
+#define VS1053_BANK_MELODY 0x79
+
+// See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 32 for more!
+#define VS1053_GM1_OCARINA 80
+
+#define MIDI_NOTE_ON  0x90
+#define MIDI_NOTE_OFF 0x80
+#define MIDI_CHAN_MSG 0xB0
+#define MIDI_CHAN_BANK 0x00
+#define MIDI_CHAN_VOLUME 0x07
+#define MIDI_CHAN_PROGRAM 0xC0
+
 
 /*
 This is the arrangment of Trellis matrices that this program is configured for.
@@ -90,7 +117,7 @@ void setup() {
   // 31250 is the baud rate for the classic serial midi protocol
   // 115200 is the default for virtual serial midi devices on computers
   Serial.begin(115200);
-
+  VS1053_MIDI.begin(31250);
   // INT pin requires a pullup
   pinMode(INTPIN, INPUT);
   digitalWrite(INTPIN, HIGH);
@@ -98,6 +125,16 @@ void setup() {
   trellis.begin(0x72, 0x71, 0x70, 0x73);
 
   runBootCheck();
+
+  pinMode(VS1053_RESET, OUTPUT);
+  digitalWrite(VS1053_RESET, LOW);
+  delay(10);
+  digitalWrite(VS1053_RESET, HIGH);
+  delay(10);
+
+  midiSetChannelBank(0, VS1053_BANK_MELODY);
+  midiSetInstrument(0, VS1053_GM1_OCARINA);
+  midiSetChannelVolume(0, 127);
 }
 
 void toggleLED(int placeVal) {
@@ -351,3 +388,50 @@ void loop() {
   delay(10); // just to be on the safe side, you can experiment with lowering this
 }
 
+void midiSetInstrument(uint8_t chan, uint8_t inst) {
+  if (chan > 15) return;
+  inst --; // page 32 has instruments starting with 1 not 0 :(
+  if (inst > 127) return;
+
+  VS1053_MIDI.write(MIDI_CHAN_PROGRAM | chan);
+  VS1053_MIDI.write(inst);
+}
+
+
+void midiSetChannelVolume(uint8_t chan, uint8_t vol) {
+  if (chan > 15) return;
+  if (vol > 127) return;
+
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write(MIDI_CHAN_VOLUME);
+  VS1053_MIDI.write(vol);
+}
+
+void midiSetChannelBank(uint8_t chan, uint8_t bank) {
+  if (chan > 15) return;
+  if (bank > 127) return;
+
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write((uint8_t)MIDI_CHAN_BANK);
+  VS1053_MIDI.write(bank);
+}
+
+void midiNoteOn(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+
+  VS1053_MIDI.write(MIDI_NOTE_ON | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
+
+void midiNoteOff(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+
+  VS1053_MIDI.write(MIDI_NOTE_OFF | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
